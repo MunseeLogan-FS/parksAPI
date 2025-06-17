91fdba1a267b55fa46469035011fd3602fa6b0c6
MunseeLogan-FS <lmmunsee@student.fullsail.edu>
2025-06-16 20:22:54 -0400

    i dont think this is it

--- api/server.js
+++ api/server.js
@@ -14,7 +14,7 @@ const nationalParkRouter = require("./routes/nationalParks");
 
 const DATABASE_URL = process.env.DATABASE_URL;
 
-mongoose.connect(process.env.DATABASE_URL, {
+mongoose.connect(DATABASE_URL, {
   useNewUrlParser: true,
 });
 
